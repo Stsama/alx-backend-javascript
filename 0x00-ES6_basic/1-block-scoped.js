a67a07f5a8a5cf4e37fdcac0a2d6393e8a878f35
StSama <albertezoula98@gmail.com>
2024-04-22 23:53:28 +0000

    task 1 fixed

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,6 +1,6 @@
 export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
+  const task = false;
+  const task2 = true;
 
   // eslint-disable-next-line no-empty
   if (trueOrFalse) {
