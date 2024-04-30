63985d78ae3345ff163f38e1454eaf34cb6edd5d
StSama <albertezoula98@gmail.com>
2024-04-30 20:30:32 +0000

    task 3 reviewed

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,5 @@
+import getListStudents from './0-get_list_students';
+
 export default function getStudentIdsSum(args) {
   if (!Array.isArray(args)) return [];
   return args.reduce((prev, next) => prev.id + next.id, 0);
