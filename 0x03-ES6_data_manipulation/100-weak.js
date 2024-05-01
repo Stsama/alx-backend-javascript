b6a4abb618c6b7f21c25ed233c7fdcfc316b8276
StSama <albertezoula98@gmail.com>
2024-05-01 18:20:37 +0000

    task 100 fixed

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -2,8 +2,8 @@ const weakMap = new WeakMap();
 
 const queryAPI = (endpoint) => {
   let total = weakMap.get(endpoint) || 0;
-  weakMap.set(endpoint, total -= 1);
-  if (total >= 5) throw Error('Endpoint load is high');
+  weakMap.set(endpoint, total -= -1);
+  if (total >= 5) throw new Error('Endpoint load is high');
   return total;
 };
 
